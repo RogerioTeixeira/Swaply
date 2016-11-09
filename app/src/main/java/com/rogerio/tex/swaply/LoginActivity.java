352e551a35b2cd61728787c8c2c4b0b28a9858b9
RogerioTeixeira <rogerio.teixeira@hotmail.it>
2016-11-09 20:25:31 +0100

    Comimit da casa

--- app/src/main/java/com/rogerio/tex/swaply/LoginActivity.java
+++ app/src/main/java/com/rogerio/tex/swaply/LoginActivity.java
@@ -8,12 +8,23 @@ import android.util.Log;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.rogerio.tex.swaply.fragment.BaseLoginFragment;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
 
 public class LoginActivity extends AppCompatActivity implements BaseLoginFragment.LoginCallback {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "vN1aPHy2T0Y8O4S6L58D6bktc";
+    private static final String TWITTER_SECRET = "91MPS4g3wmHcWz1WdjHiN4ifxMDHXvgNmCTYuTMBR07ixXICwU";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
     }
 
