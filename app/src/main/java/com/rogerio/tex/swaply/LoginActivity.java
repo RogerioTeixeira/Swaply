ac136a6f365f5c87ef2ee4440be8e870033760c2
RogerioTeixeira <rogerio.teixeira@hotmail.it>
2016-11-09 23:34:54 +0100

    Comimit da casa

--- app/src/main/java/com/rogerio/tex/swaply/LoginActivity.java
+++ app/src/main/java/com/rogerio/tex/swaply/LoginActivity.java
@@ -4,20 +4,31 @@ package com.rogerio.tex.swaply;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.rogerio.tex.swaply.fragment.BaseLoginFragment;
 import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.fabric.sdk.android.Fabric;
+
 
 public class LoginActivity extends AppCompatActivity implements BaseLoginFragment.LoginCallback {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "vN1aPHy2T0Y8O4S6L58D6bktc";
     private static final String TWITTER_SECRET = "91MPS4g3wmHcWz1WdjHiN4ifxMDHXvgNmCTYuTMBR07ixXICwU";
+    @BindView(R.id.twitter_login_button)
+    TwitterLoginButton twitterLoginButton;
 
 
     @Override
@@ -26,6 +37,28 @@ public class LoginActivity extends AppCompatActivity implements BaseLoginFragmen
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+                TwitterSession session = result.data;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
+
+
     }
 
     @Override
