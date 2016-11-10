8c067ef1ac69d63fcf38268144188fd1fbc376ed
Rogerio Lavoro <Rogerio Lavoro>
2016-11-10 18:32:18 +0100

    Commit allianz

--- app/src/main/java/com/rogerio/tex/swaply/LoginActivity.java
+++ app/src/main/java/com/rogerio/tex/swaply/LoginActivity.java
@@ -1,64 +1,33 @@
 package com.rogerio.tex.swaply;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.rogerio.tex.swaply.fragment.BaseLoginFragment;
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.Callback;
-import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterException;
-import com.twitter.sdk.android.core.TwitterSession;
-import com.twitter.sdk.android.core.identity.TwitterLoginButton;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import io.fabric.sdk.android.Fabric;
 
 
 public class LoginActivity extends AppCompatActivity implements BaseLoginFragment.LoginCallback {
 
-    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "vN1aPHy2T0Y8O4S6L58D6bktc";
-    private static final String TWITTER_SECRET = "91MPS4g3wmHcWz1WdjHiN4ifxMDHXvgNmCTYuTMBR07ixXICwU";
-    @BindView(R.id.twitter_login_button)
-    TwitterLoginButton twitterLoginButton;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
-        ButterKnife.bind(this);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
-            @Override
-            public void success(Result<TwitterSession> result) {
-                // The TwitterSession is also available through:
-                // Twitter.getInstance().core.getSessionManager().getActiveSession()
-                TwitterSession session = result.data;
-                // TODO: Remove toast and use the TwitterSession's userID
-                // with your app's user model
-                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void failure(TwitterException exception) {
-                Log.d("TwitterKit", "Login with Twitter failure", exception);
-            }
-        });
-
-
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_login_twitter);
+        if (fragment != null) {
+            fragment.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
--- app/src/main/java/com/rogerio/tex/swaply/fragment/TwitterLoginFragment.java
+++ app/src/main/java/com/rogerio/tex/swaply/fragment/TwitterLoginFragment.java
@@ -1,13 +1,26 @@
 package com.rogerio.tex.swaply.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.rogerio.tex.swaply.R;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import io.fabric.sdk.android.Fabric;
 
 
 /**
@@ -15,6 +28,16 @@ import com.rogerio.tex.swaply.R;
  */
 public class TwitterLoginFragment extends BaseLoginFragment {
 
+    private static final String TWITTER_KEY = "vN1aPHy2T0Y8O4S6L58D6bktc";
+    private static final String TWITTER_SECRET = "91MPS4g3wmHcWz1WdjHiN4ifxMDHXvgNmCTYuTMBR07ixXICwU";
+    @BindView(R.id.sign_in_button_twitter)
+    Button signInButtonTwitter;
+    @BindView(R.id.disconnect_button_twitter)
+    Button disconnectButtonTwitter;
+
+    @BindView(R.id.twitter_login_button)
+    TwitterLoginButton twitterLoginButton;
+    private TwitterAuthClient client;
 
     public TwitterLoginFragment() {
         // Required empty public constructor
@@ -22,16 +45,60 @@ public class TwitterLoginFragment extends BaseLoginFragment {
 
     @Override
     protected int getFragmentLayout() {
-
         return R.layout.fragment_twitter_login;
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_twitter_login, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(getContext(), new Twitter(authConfig));
+
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                Log.d("TwitterKit", "login success");
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login failure");
+            }
+        });
+
+    }
+
+
+    @OnClick({R.id.sign_in_button_twitter, R.id.disconnect_button_twitter})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button_twitter:
+                signIn();
+                break;
+            case R.id.disconnect_button_twitter:
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void signIn() {
+        twitterLoginButton.performClick();
+    }
 }
